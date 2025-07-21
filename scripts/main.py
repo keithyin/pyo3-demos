b5e3e500b9202989fb173113863a71187e9519a5
RocYin <peng.yin@geneus-tech.com>
2025-07-21 09:03:20 +0000

    numpy

--- scripts/main.py
+++ scripts/main.py
@@ -1,5 +1,15 @@
 import pyo3_demos
 import numpy as np
 
-print(pyo3_demos.sum_as_string(1, 2))
 
+def main():
+    print(pyo3_demos.sum_as_string(1, 2))
+    value = np.array([[2.0, 3.0]], dtype=np.float64)
+    pyo3_demos.square_in_place(value)
+    print(value)
+    pass
+
+
+if __name__ == "__main__":
+    main()
+    pass
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use numpy::PyReadonlyArray2;
+use numpy::{PyReadonlyArray2, PyReadwriteArray2};
 use pyo3::prelude::*;
 
 /// Formats the sum of two numbers as string.
@@ -12,6 +12,13 @@ fn matrix_point_product_and_sum(arr: PyReadonlyArray2<f64>) -> f64 {
     let arr = arr.as_array();
     arr.map(|&x| (x * x)).sum()
 }
+
+#[pyfunction]
+fn square_in_place(mut arr: PyReadwriteArray2<f64>) {
+    let mut arr = arr.as_array_mut();
+    arr.mapv_inplace(|v| v * v);
+}
+
 #[pyfunction]
 fn matrix_point_product_and_sum_without_gil(py: Python<'_>, arr: PyReadonlyArray2<f64>) -> f64 {
     let arr = arr.as_array();
@@ -23,6 +30,12 @@ fn matrix_point_product_and_sum_without_gil(py: Python<'_>, arr: PyReadonlyArray
 fn pyo3_demos(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
     m.add_function(wrap_pyfunction!(matrix_point_product_and_sum, m)?)?;
-    m.add_function(wrap_pyfunction!(matrix_point_product_and_sum_without_gil, m)?)?;
+    m.add_function(wrap_pyfunction!(
+        matrix_point_product_and_sum_without_gil,
+        m
+    )?)?;
+
+    m.add_function(wrap_pyfunction!(square_in_place, m)?)?;
+
     Ok(())
 }
